424dbb1b5462bcb789468095df25e00e52e7c2ce
chirag <chiragsharma1676@gmail.com>
2023-12-10 10:34:46 +0530

    added deployed url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL
-const BASE_URL = `https://study-notion-backend-gw2w.onrender.com`;
+const BASE_URL = `https://study-notion-backend-gw2w.onrender.com/api/v1`;
 
 // AUTH ENDPOINTS
 export const endpoints = {
